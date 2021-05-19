62ae4fbe256e4e296196244b61b588b62411cd98
JBD <lio222@hanmail.net>
2021-05-19 14:37:04 +0900

    2021-05-19 SpringMVC Log4jdbc-log4j2 Configuration

--- ex00/src/test/java/org/zerock/persistence/JDBCTests.java
+++ ex00/src/test/java/org/zerock/persistence/JDBCTests.java
@@ -10,10 +10,18 @@ import lombok.extern.log4j.Log4j;
 
 @Log4j
 public class JDBCTests {
-
+//
+//	static {
+//		try {
+//			Class.forName("oracle.jdbc.driver.OracleDriver"); 
+//			
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//		}
+//	}
 	static {
 		try {
-			Class.forName("oracle.jdbc.driver.OracleDriver"); 
+			Class.forName("net.sf.log4jdbc.sql.jdbcapi.DriverSpy"); 
 			
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -21,10 +29,22 @@ public class JDBCTests {
 	}
 	
 	@Test
+//	public void testConnetion() {
+//		try (Connection con = 
+//				DriverManager.getConnection(
+//						"jdbc:oracle:thin:@localhost:1521:XE",
+//						"scott",
+//						"tiger")){
+//			log.info(con);
+//		} catch (Exception e) {
+//			fail(e.getMessage());
+//		}
+//	}
+	
 	public void testConnetion() {
 		try (Connection con = 
 				DriverManager.getConnection(
-						"jdbc:oracle:thin:@localhost:1521:XE",
+						"jdbc:log4jdbc:oracle:thin:@localhost:1521:XE",
 						"scott",
 						"tiger")){
 			log.info(con);
@@ -32,4 +52,5 @@ public class JDBCTests {
 			fail(e.getMessage());
 		}
 	}
+
 }
