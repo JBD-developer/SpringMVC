b9941a5d405cbd2f2f78589ae25c5d6bd2c1c940
JBD <lio222@hanmail.net>
2021-12-01 20:04:56 +0900

    2021-12-01 SpringMVC Connect JDBC

---
+++ ex05/src/test/java/zerock/org/persistence/JDBCTest2.java
@@ -0,0 +1,34 @@
+package zerock.org.persistence;
+import static  org.junit.Assert.fail;
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+import org.junit.Test;
+
+import lombok.extern.log4j.Log4j;
+
+@Log4j
+public class JDBCTest2 {
+
+	static {
+	
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+	}
+	
+	@Test
+	public void testConnction() {
+		try(Connection con = DriverManager.getConnection(
+				"jdbc:oracle:thin:@localhost:1521:XE",
+				"scott",
+				"1234")){
+			log.info(con);
+			
+		} catch (Exception ex) {
+			fail(ex.getMessage());
+		}
+	}
+}
